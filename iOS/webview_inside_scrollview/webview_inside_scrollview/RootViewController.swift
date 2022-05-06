e0ab7fc4acff6c976edffd92e8ad8f54f306e075
WingCH <wingch.apps@gmail.com>
2022-05-06 20:47:46 +0800

    [webview_inside_scrollview] init project

---
+++ iOS/webview_inside_scrollview/webview_inside_scrollview/RootViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RootViewController.swift
+//  webview_inside_scrollview
+//
+//  Created by Wing on 6/5/2022.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}

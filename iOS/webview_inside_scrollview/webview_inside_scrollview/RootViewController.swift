ecc3218195966b2b945802bf08cea02ad789ba52
WingCH <wingch.apps@gmail.com>
2022-05-07 10:55:23 +0800

    [webview_inside_scrollview] setup scroll view

--- iOS/webview_inside_scrollview/webview_inside_scrollview/RootViewController.swift
+++ iOS/webview_inside_scrollview/webview_inside_scrollview/RootViewController.swift
@@ -5,61 +5,90 @@
 //  Created by Wing on 6/5/2022.
 //
 
-import UIKit
 import TinyConstraints
+import UIKit
 
 class RootViewController: UIViewController {
-    
     let scrollView: UIScrollView = {
         let v = UIScrollView()
         v.backgroundColor = .cyan
         return v
     }()
-    
+
     let scrollContentView = UIView()
     let contentStackView = UIStackView()
-    
+
     let view1: UIView = {
         let view = UIView()
         view.backgroundColor = .red
         return view
     }()
-    
+
     let view2: UIView = {
         let view = UIView()
         view.backgroundColor = .yellow
         return view
     }()
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         addSubViews()
         constrainSubviews()
     }
-    
+
     fileprivate func addSubViews() {
         view.addSubview(scrollView)
         scrollView.addSubview(scrollContentView)
         scrollContentView.addSubview(contentStackView)
-        
+
         contentStackView.addArrangedSubview(view1)
         contentStackView.addArrangedSubview(view2)
     }
-    
+
     fileprivate func constrainSubviews() {
         view.backgroundColor = .white
-        
+
         scrollView.edgesToSuperview()
+
+        scrollContentView.backgroundColor = .green
         scrollContentView.edgesToSuperview()
         scrollContentView.widthToSuperview()
-        
+
+        contentStackView.backgroundColor = .gray
         contentStackView.axis = .vertical
         contentStackView.edgesToSuperview()
-        
+
         view1.height(100)
-        
-        view2.widthToSuperview()
-        view2.height(1000)
+        view2.height(1500)
     }
+
+    /*
+         fileprivate func addSubViews() {
+             view.addSubview(scrollView)
+             scrollView.addSubview(contentStackView)
+     //        scrollView.addSubview(scrollContentView)
+     //        scrollContentView.addSubview(contentStackView)
+     //
+             contentStackView.addArrangedSubview(view1)
+             contentStackView.addArrangedSubview(view2)
+         }
+
+         fileprivate func constrainSubviews() {
+             view.backgroundColor = .white
+
+             scrollView.edgesToSuperview()
+
+     //        scrollContentView.backgroundColor = .green
+
+     //        scrollContentView.edgesToSuperview()
+     //        scrollContentView.widthToSuperview()
+     //        scrollContentView.heightToSuperview()
+
+             contentStackView.axis = .vertical
+             contentStackView.widthToSuperview()
+
+             view1.height(100)
+             view2.height(1500)
+         }
+         */
 }

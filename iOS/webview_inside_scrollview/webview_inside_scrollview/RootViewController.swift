6646c6ded4460220e74218c4f70127cf91d7f8e6
WingCH <wingch.apps@gmail.com>
2022-05-07 08:24:24 +0800

    [webview_inside_scrollview] setup basic view

--- iOS/webview_inside_scrollview/webview_inside_scrollview/RootViewController.swift
+++ iOS/webview_inside_scrollview/webview_inside_scrollview/RootViewController.swift
@@ -6,10 +6,43 @@
 //
 
 import UIKit
+import TinyConstraints
 
 class RootViewController: UIViewController {
+    
+    let view1: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    let view2: UIView = {
+        let view = UIView()
+        view.backgroundColor = .yellow
+        return view
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        addSubViews()
+        constrainSubviews()
+    }
+    
+    fileprivate func addSubViews() {
+        view.addSubview(view1)
+        view.addSubview(view2)
+    }
+    
+    fileprivate func constrainSubviews() {
+        view.backgroundColor = .white
+        
+        view1.edgesToSuperview(excluding: .bottom, usingSafeArea: true)
+        view1.height(100)
+        
+        view2.topToBottom(of: view1)
+        view2.leftToSuperview()
+        view2.rightToSuperview()
+        view2.height(100)
     }
 }

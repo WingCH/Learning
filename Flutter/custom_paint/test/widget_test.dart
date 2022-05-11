a51ad8c73cf404c4ea76a8d9d72d2e1e8dd3974a
WingCH <wingch.apps@gmail.com>
2022-05-11 20:33:00 +0800

    [custom_paint] init

--- Flutter/custom_paint/test/widget_test.dart
+++ Flutter/custom_paint/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:custom_paint/main.dart';
+import 'package:custom_paint/qr_scanner_corners/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

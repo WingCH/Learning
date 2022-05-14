67ef2919e6d2a5bae215216d0509a69120be2752
WingCH <wingch.apps@gmail.com>
2022-05-14 11:01:55 +0800

    [sync_ios_setting_languages] update

--- Flutter/sync_ios_setting_languages/lib/home_page.dart
+++ Flutter/sync_ios_setting_languages/lib/home_page.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:native_shared_preferences/native_shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,19 +12,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<String> languages = [];
+  final String defaultLocale = Platform.localeName;
+  final List<Locale> systemLocales = window.locales;
+  List<String> appleLanguages = [];
 
   @override
   void initState() {
     super.initState();
-    fetch();
+    getAppleLanguages();
   }
 
-  Future<void> fetch() async {
-    final prefs = await SharedPreferences.getInstance();
+  Future<void> getAppleLanguages() async {
+    final prefs = await NativeSharedPreferences.getInstance();
     setState(() {
-      languages = prefs.getStringList('AppleLanguages') ?? [];
+      appleLanguages = prefs.getStringList('AppleLanguages') ?? [];
     });
+  }
+
+  Future<void> setAppleLanguages() async {
+    // This plugin is a copy of the shared_preferences package but without the prefix in the keys
+    final prefs = await NativeSharedPreferences.getInstance();
+    // https://docs.flutter.dev/development/accessibility-and-localization/internationalization#specifying-supportedlocales
+    await prefs.setStringList('AppleLanguages', <String>[
+      const Locale.fromSubtags(languageCode: 'zh', scriptCode: 'Hant')
+          .toString()
+    ]);
+
+    /*
+    Apple Locale format:
+    zh-Hant
+    flutter locale toString format:
+    zh_Hant
+     */
   }
 
   @override
@@ -32,8 +54,29 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-             Text('AppleLanguages', style: Theme.of(context).textTheme.titleSmall,),
-            for (var language in languages) Text(language),
+            Text(
+              'defaultLocale',
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+            Text(defaultLocale),
+            Text(
+              'systemLocales (PREFERRED LANGUAGE ORDER)',
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+            Text(systemLocales.toString()),
+            Text(
+              'AppleLanguages',
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+            Text(appleLanguages.toString()),
+            TextButton(
+              onPressed: setAppleLanguages,
+              child: const Text('Set AppleLanguages'),
+            ),
+            TextButton(
+              onPressed: getAppleLanguages,
+              child: const Text('Get AppleLanguages'),
+            )
           ],
         ),
       ),

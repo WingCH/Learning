2e82867adf729e2109c0ea92e8fb1f9f8e582271
WingCH <wingch.apps@gmail.com>
2022-05-25 22:52:52 +0800

    [rxdart] update logic

--- Flutter/rxdart/lib/library_service.dart
+++ Flutter/rxdart/lib/library_service.dart
@@ -1,47 +1,76 @@
-import 'dart:ui';
-
 import 'package:rxdart/rxdart.dart';
 
 class Book {
+  final String id;
   final String name;
-  final Color color;
 
-  Book({required this.name, required this.color});
+  Book({required this.id, required this.name});
 
   @override
   String toString() {
     return 'Book('
-        'name: $name, '
-        'color: $color'
+        'id: $id, '
+        'name: $name'
         ')';
   }
 
   @override
   bool operator ==(Object other) {
-    return other is Book && other.name == name && other.color == color;
+    return other is Book && other.id == id && other.name == name;
   }
 
   @override
   int get hashCode {
     return Object.hash(
       runtimeType,
+      id,
       name,
-      color,
     );
   }
 }
 
-// https://stackoverflow.com/a/12649574/5588637
+// https://flutter.cn/community/tutorials/singleton-pattern-in-flutter-n-dart
 class LibraryService {
-  static final LibraryService _singleton = LibraryService._internal();
-
-  factory LibraryService() {
-    return _singleton;
+  LibraryService._internal() {
+    _booksSubject.listen((value) {
+      print('listen: $value');
+    });
   }
 
-  LibraryService._internal();
+  factory LibraryService() => _instance;
+
+  static final LibraryService _instance = LibraryService._internal();
 
   final _booksSubject = BehaviorSubject<List<Book>>.seeded([]);
+
+  Stream<List<Book>> get stream => _booksSubject.stream;
+
+  void setup() {
+    _booksSubject.value = [
+      Book(
+        id: "1",
+        name: 'iOS 13 App程式設計實戰心法',
+      ),
+      Book(
+        id: "2",
+        name: 'Swift 進階',
+      ),
+      Book(
+        id: "3",
+        name: 'Getting Started with the BLoC Pattern',
+      ),
+      Book(
+        id: "4",
+        name: 'Parsing JSON in Flutter',
+      ),
+    ];
+  }
+
+  void addBook({required Book book}) {
+    List<Book> oldList = _booksSubject.value.toList();
+    oldList.add(book);
+    _booksSubject.value = oldList;
+  }
 
   /// 借書
   void checkIn({required String bookName}) {}

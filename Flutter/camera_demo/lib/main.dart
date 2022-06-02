710a70b078592db9b7cd082cacab8ee291350fd1
Wing Chan <wing.chan@keysocapp.com>
2022-06-02 18:15:13 +0800

    [camera_demo] Preliminary example

--- Flutter/camera_demo/lib/main.dart
+++ Flutter/camera_demo/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_selfie_segmentation/google_mlkit_selfie_segmentation.dart';
+
+import 'segmentation_painter.dart';
 
 late List<CameraDescription> _cameras;
 
@@ -22,17 +26,88 @@ class CameraApp extends StatefulWidget {
 class _CameraAppState extends State<CameraApp> {
   late CameraController controller;
 
+  final SelfieSegmenter _segmenter = SelfieSegmenter(
+    mode: SegmenterMode.stream,
+    enableRawSizeMask: true,
+  );
+  bool _isBusy = false;
+  CustomPaint? _customPaint;
+
   void receivedImage(CameraImage image) {
     print("receivedImage: $image");
+
+    final WriteBuffer allBytes = WriteBuffer();
+    for (final Plane plane in image.planes) {
+      allBytes.putUint8List(plane.bytes);
+    }
+    final bytes = allBytes.done().buffer.asUint8List();
+
+    final Size imageSize =
+        Size(image.width.toDouble(), image.height.toDouble());
+
+    final camera = _cameras[1];
+    final imageRotation =
+        InputImageRotationValue.fromRawValue(camera.sensorOrientation) ??
+            InputImageRotation.rotation0deg;
+
+    final inputImageFormat =
+        InputImageFormatValue.fromRawValue(image.format.raw) ??
+            InputImageFormat.nv21;
+
+    final planeData = image.planes.map(
+      (Plane plane) {
+        return InputImagePlaneMetadata(
+          bytesPerRow: plane.bytesPerRow,
+          height: plane.height,
+          width: plane.width,
+        );
+      },
+    ).toList();
+
+    final inputImageData = InputImageData(
+      size: imageSize,
+      imageRotation: imageRotation,
+      inputImageFormat: inputImageFormat,
+      planeData: planeData,
+    );
+
+    final inputImage =
+        InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
+
+    processImage(inputImage);
   }
 
-  
+  Future<void> processImage(InputImage inputImage) async {
+    if (_isBusy) return;
+    _isBusy = true;
+
+    final mask = await _segmenter.processImage(inputImage);
+    if (inputImage.inputImageData?.size != null &&
+        inputImage.inputImageData?.imageRotation != null &&
+        mask != null) {
+      // TODO: Fix SegmentationPainter to rescale on top of camera feed.
+      final painter = SegmentationPainter(
+        mask,
+        inputImage.inputImageData!.size,
+        inputImage.inputImageData!.imageRotation,
+      );
+      _customPaint = CustomPaint(painter: painter);
+    } else {
+      // TODO: set _customPaint to draw on top of image
+      _customPaint = null;
+    }
+    _isBusy = false;
+    if (mounted) {
+      setState(() {});
+    }
+  }
 
   @override
   void initState() {
     super.initState();
+
     controller =
-        CameraController(_cameras[0], ResolutionPreset.max, enableAudio: false);
+        CameraController(_cameras[1], ResolutionPreset.max, enableAudio: false);
     controller.initialize().then((_) {
       if (!mounted) {
         return;
@@ -67,7 +142,17 @@ class _CameraAppState extends State<CameraApp> {
       return Container();
     }
     return MaterialApp(
-      home: CameraPreview(controller),
+      home: Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            CameraPreview(
+              controller,
+            ),
+            if (_customPaint != null) _customPaint!
+          ],
+        ),
+      ),
     );
   }
 }

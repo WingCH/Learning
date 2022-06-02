ea1662891281818702bcd3fbb5fca03b30603137
Wing Chan <wing.chan@keysocapp.com>
2022-06-02 18:23:03 +0800

    [camera_demo] rename

--- Flutter/camera_demo/test/widget_test.dart
+++ Flutter/camera_demo/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:camera_demo/main.dart';
+import 'package:camera_demo/selfie_segmentation_main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

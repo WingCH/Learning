9e70e4d1e9120ea0f8e9a065a2ee3136de3861ba
WingCH <wingch.apps@gmail.com>
2022-06-19 15:43:36 +0800

    [iOS][carousel] init project

---
+++ iOS/carousel/carousel/RootViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RootViewController.swift
+//  carousel
+//
+//  Created by Wing on 19/6/2022.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}

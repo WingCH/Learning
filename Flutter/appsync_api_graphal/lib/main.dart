33470881552b62ad9f081794a68452fd519c95aa
Wing CHAN <wingch.apps@gmail.com>
2022-07-09 00:38:04 +0800

    [appsync_api_graphal] success listen

--- Flutter/appsync_api_graphal/lib/main.dart
+++ Flutter/appsync_api_graphal/lib/main.dart
@@ -5,7 +5,6 @@ import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 
 import 'amplifyconfiguration.dart';
-import 'models/ModelProvider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -44,42 +43,11 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> _configureAmplify() async {
-    final api = AmplifyAPI(modelProvider: ModelProvider.instance);
+    final api = AmplifyAPI();
     await Amplify.addPlugin(api);
 
     try {
       await Amplify.configure(amplifyconfig);
-
-      // try {
-      //   final channel = Channel(name: 'channel', data: "{\"msg\": \"hello world!\"}");
-      //   final request = ModelMutations.create(channel);
-      //   final response = await Amplify.API.mutate(request: request).response;
-      //
-      //   final createdTodo = response.data;
-      //   if (createdTodo == null) {
-      //     print('errors: ${response.errors}');
-      //     return;
-      //   }
-      //   print('Mutation result: ${createdTodo.name}');
-      // } on ApiException catch (e) {
-      //   print('Mutation failed: $e');
-      // }
-
-      // final subscriptionRequest = ModelSubscriptions.onCreate(Channel.classType);
-      // final Stream<GraphQLResponse<Channel>> operation = Amplify.API.subscribe(
-      //   subscriptionRequest,
-      //   onEstablished: () {
-      //     print('Subscription established');
-      //   },
-      // );
-      // subscription =  operation.listen(
-      //       (event) {
-      //     print('Subscription event data received: ${event.data}');
-      //   },
-      //   onError: (Object e) {
-      //     print('Error in subscription stream: $e');
-      //   },
-      // );
 
       // https://docs.amplify.aws/guides/api-graphql/subscriptions-by-id/q/platform/flutter/
       const graphQLDocument = r'''

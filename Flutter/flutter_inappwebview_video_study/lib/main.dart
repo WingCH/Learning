5faee74255eb24a18b2bddcbb7aebe74f613a985
Wing CHAN <wingch.apps@gmail.com>
2022-08-07 10:42:35 +0800

    [flutter_inappwebview_video_study] init sample code

--- Flutter/flutter_inappwebview_video_study/lib/main.dart
+++ Flutter/flutter_inappwebview_video_study/lib/main.dart
@@ -1,6 +1,13 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+  }
   runApp(const MyApp());
 }
 
@@ -27,8 +34,102 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final GlobalKey webViewKey = GlobalKey();
+
+  InAppWebViewController? webViewController;
+  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+    crossPlatform: InAppWebViewOptions(
+      useShouldOverrideUrlLoading: true,
+      mediaPlaybackRequiresUserGesture: false,
+    ),
+    android: AndroidInAppWebViewOptions(
+      useHybridComposition: true,
+    ),
+    ios: IOSInAppWebViewOptions(
+      allowsInlineMediaPlayback: true,
+    ),
+  );
+
+  String url = "";
+  double progress = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Official InAppWebView website")),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Stack(
+                children: [
+                  InAppWebView(
+                    key: webViewKey,
+                    initialUrlRequest: URLRequest(url: Uri.parse("https://inappwebview.dev/")),
+                    initialOptions: options,
+                    onWebViewCreated: (controller) {
+                      webViewController = controller;
+                    },
+                    onLoadStart: (controller, url) {
+                      print('${DateTime.now()} [debug][onLoadStart]: url: $url');
+                    },
+                    shouldOverrideUrlLoading: (controller, navigationAction) async {
+                      return NavigationActionPolicy.ALLOW;
+                    },
+                    onLoadStop: (controller, url) async {
+                      print('${DateTime.now()} [debug][onLoadStop]: url: $url');
+                    },
+                    onLoadError: (controller, url, code, message) {
+                      print('${DateTime.now()} [debug][onLoadError]: url: $url\ncode: $code\nmessage: $message');
+                    },
+                    onProgressChanged: (controller, progress) {
+                      print('${DateTime.now()} [debug][onProgressChanged]: progress: $progress');
+                      setState(() {
+                        this.progress = progress / 100;
+                      });
+                    },
+                    onUpdateVisitedHistory: (controller, url, androidIsReload) {
+                      print('${DateTime.now()} [debug][onUpdateVisitedHistory]: url: $url');
+                    },
+                    onConsoleMessage: (controller, consoleMessage) {
+                      print('${DateTime.now()} [debug][onConsoleMessage] $consoleMessage');
+                    },
+                  ),
+                  Stack(
+                    children: [
+                      LinearProgressIndicator(
+                        value: progress,
+                      ),
+                      Text(progress.toString()),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  child: const Icon(Icons.refresh),
+                  onPressed: () {
+                    webViewController?.reload();
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

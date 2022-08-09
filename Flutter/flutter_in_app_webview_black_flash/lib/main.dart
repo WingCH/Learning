0aef01fd30b4285e5493ed59309aefcdf7866b13
Wing CHAN <wingch.apps@gmail.com>
2022-08-09 21:07:12 +0800

    [flutter_in_app_webview_black_flash] update

--- Flutter/flutter_in_app_webview_black_flash/lib/main.dart
+++ Flutter/flutter_in_app_webview_black_flash/lib/main.dart
@@ -44,8 +44,14 @@ class MyHomePage extends StatelessWidget {
             onTap: () {
               Navigator.push(
                 context,
-                CupertinoPageRoute(
-                  builder: (context) => ExamplePage(),
+                // CupertinoPageRoute(
+                //   builder: (context) => ExamplePage(),
+                // ),
+                // MaterialPageRoute(
+                //   builder: (context) => ExamplePage(),
+                // ),
+                CustomTransitionRoute(
+                  builder: (context) => const ExamplePage(),
                 ),
               );
             },
@@ -54,4 +60,27 @@ class MyHomePage extends StatelessWidget {
       ),
     );
   }
+}
+
+// https://github.com/pichillilorenzo/flutter_inappwebview/issues/1098
+class CustomTransitionRoute extends CupertinoPageRoute<void> {
+  CustomTransitionRoute({
+    required WidgetBuilder builder,
+    RouteSettings? settings,
+  }) : super(builder: builder, settings: settings, fullscreenDialog: false);
+
+  @override
+  bool get opaque => true;
+
+  @override
+  Color? get barrierColor => null;
+
+  @override
+  String? get barrierLabel => null;
+
+  @override
+  bool get maintainState => true;
+
+  @override
+  Duration get transitionDuration => Duration(seconds: 5);
 }

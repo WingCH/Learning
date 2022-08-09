ea3af7d826d830679785c4feabc64ca269b0a7d4
Wing Chan <wing.chan@keysocapp.com>
2022-08-09 10:55:16 +0800

    [timezone_study] init project and add flutter_native_timezone_example

---
+++ Flutter/timezone_study/android/app/src/main/kotlin/com/example/timezone_study/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.timezone_study
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/timezone_study/lib/flutter_native_timezone_example.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
+
+class FlutterNativeTimezoneExample extends StatefulWidget {
+  const FlutterNativeTimezoneExample({Key? key}) : super(key: key);
+
+  @override
+  State<FlutterNativeTimezoneExample> createState() =>
+      _FlutterNativeTimezoneExampleState();
+}
+
+class _FlutterNativeTimezoneExampleState
+    extends State<FlutterNativeTimezoneExample> {
+  String _timezone = 'Unknown';
+  List<String> _availableTimezones = <String>[];
+
+  @override
+  void initState() {
+    super.initState();
+    _initData();
+  }
+
+  Future<void> _initData() async {
+    try {
+      _timezone = await FlutterNativeTimezone.getLocalTimezone();
+    } catch (e) {
+      print('Could not get the local timezone');
+    }
+    try {
+      _availableTimezones = await FlutterNativeTimezone.getAvailableTimezones();
+      _availableTimezones.sort();
+    } catch (e) {
+      print('Could not get available timezones');
+    }
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Local timezone app'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text('Local timezone: $_timezone\n'),
+            const Text('Available timezones:'),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _availableTimezones.length,
+                itemBuilder: (_, index) => Text(_availableTimezones[index]),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

602419dd587a07f32233da1e9e205e0559e799f9
Wing Chan <wing.chan@keysocapp.com>
2022-09-09 17:25:53 +0800

    [study_visitor_patterns] done

--- Flutter/study_visitor_patterns/bin/study_visitor_patterns.dart
+++ Flutter/study_visitor_patterns/bin/study_visitor_patterns.dart
@@ -1,5 +1,67 @@
-import 'package:study_visitor_patterns/study_visitor_patterns.dart' as study_visitor_patterns;
+import 'package:study_visitor_patterns/study_visitor_patterns.dart'
+    as study_visitor_patterns;
+
+abstract class BookmarkReferenceVisitor<T> {
+  T forId(BookmarkReferenceId item);
+
+  T forSlug(BookmarkReferenceSlug item);
+}
+
+class SimpleBookmarkReferenceVisitor<T> implements BookmarkReferenceVisitor<T> {
+  final T Function(BookmarkReferenceId) idHandler;
+  final T Function(BookmarkReferenceSlug) slugHandler;
+
+  const SimpleBookmarkReferenceVisitor({
+    required this.idHandler,
+    required this.slugHandler,
+  });
+
+  @override
+  T forId(BookmarkReferenceId item) => idHandler(item);
+
+  @override
+  T forSlug(BookmarkReferenceSlug item) => slugHandler(item);
+}
+
+abstract class BookmarkReferenceType {
+  T accept<T>(BookmarkReferenceVisitor<T> visitor);
+}
+
+class BookmarkReferenceId extends BookmarkReferenceType {
+  final int value;
+
+  BookmarkReferenceId(this.value);
+
+  @override
+  T accept<T>(BookmarkReferenceVisitor<T> visitor) {
+    return visitor.forId(this);
+  }
+}
+
+class BookmarkReferenceSlug extends BookmarkReferenceType {
+  final String value;
+
+  BookmarkReferenceSlug(this.value);
+
+  @override
+  T accept<T>(BookmarkReferenceVisitor<T> visitor) {
+    return visitor.forSlug(this);
+  }
+}
 
 void main(List<String> arguments) {
-  print('Hello world: ${study_visitor_patterns.calculate()}!');
+  final parameter = {};
+
+  BookmarkReferenceId id = BookmarkReferenceId(1);
+  BookmarkReferenceSlug slug = BookmarkReferenceSlug('slug');
+
+  BookmarkReferenceVisitor<void> visitor = SimpleBookmarkReferenceVisitor(
+    idHandler: (id) => parameter['id'] = id.value,
+    slugHandler: (slug) => parameter['id'] = slug.value,
+  );
+
+  // id.accept(visitor);
+  slug.accept(visitor);
+
+  print(parameter);
 }

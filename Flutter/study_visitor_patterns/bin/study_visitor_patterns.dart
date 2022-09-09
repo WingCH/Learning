92b8a760f1e50df44e276a196a1af4c6277bd168
Wing Chan <wing.chan@keysocapp.com>
2022-09-09 16:55:13 +0800

    [study_visitor_patterns] init project

---
+++ Flutter/study_visitor_patterns/bin/study_visitor_patterns.dart
@@ -0,0 +1,5 @@
+import 'package:study_visitor_patterns/study_visitor_patterns.dart' as study_visitor_patterns;
+
+void main(List<String> arguments) {
+  print('Hello world: ${study_visitor_patterns.calculate()}!');
+}
---
+++ Flutter/study_visitor_patterns/test/study_visitor_patterns_test.dart
@@ -0,0 +1,8 @@
+import 'package:study_visitor_patterns/study_visitor_patterns.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

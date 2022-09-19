9aacac0001b728aab798a1d7b5cd4912973474e8
Wing Chan <wing.chan@keysocapp.com>
2022-09-19 17:36:01 +0800

    Revert "[study_generic_type)"
    This reverts commit 25268d1830c20b6be423cbe93fc1cfda31dd5335.

--- Flutter/study_generic_type/bin/study_generic_type.dart
+++
@@ -1,8 +0,0 @@
-import 'package:study_generic_type/study_generic_type.dart' as study_generic_type;
-
-
-
-
-void main(List<String> arguments) {
-  print('Hello world: ${study_generic_type.calculate()}!');
-}
--- Flutter/study_generic_type/test/study_generic_type_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:study_generic_type/study_generic_type.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}

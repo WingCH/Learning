25268d1830c20b6be423cbe93fc1cfda31dd5335
Wing Chan <wing.chan@keysocapp.com>
2022-09-19 14:23:14 +0800

    [study_generic_type)

---
+++ Flutter/study_generic_type/bin/study_generic_type.dart
@@ -0,0 +1,8 @@
+import 'package:study_generic_type/study_generic_type.dart' as study_generic_type;
+
+
+
+
+void main(List<String> arguments) {
+  print('Hello world: ${study_generic_type.calculate()}!');
+}
---
+++ Flutter/study_generic_type/test/study_generic_type_test.dart
@@ -0,0 +1,8 @@
+import 'package:study_generic_type/study_generic_type.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

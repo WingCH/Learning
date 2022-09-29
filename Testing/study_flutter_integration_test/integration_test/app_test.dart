4a9b89252f703f784c44a9890b1dfdf7d844db38
Wing Chan <wing.chan@keysocapp.com>
2022-09-29 10:15:16 +0800

    [Testing][study_flutter_integration_test] init project

---
+++ Testing/study_flutter_integration_test/integration_test/app_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:study_flutter_integration_test/main.dart' as app;
+
+// https://docs.flutter.dev/cookbook/testing/integration/introduction
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('tap on the floating action button, verify counter',
+        (tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      // Verify the counter starts at 0.
+      expect(find.text('0'), findsOneWidget);
+
+      // Finds the floating action button to tap on.
+      // final Finder fab = find.byTooltip('Increment');
+      final Finder fab = find.byKey(const Key('increment'));
+
+      // Emulate a tap on the floating action button.
+      await tester.tap(fab);
+
+      // Trigger a frame.
+      await tester.pumpAndSettle();
+
+      // Verify the counter increments by 1.
+      expect(find.text('1'), findsOneWidget);
+    });
+  });
+}

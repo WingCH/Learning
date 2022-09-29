6ad18153f9ad3d0ead35b28a50b8dd19b1463a8e
Wing Chan <wing.chan@keysocapp.com>
2022-09-29 10:36:14 +0800

    [Testing][appium][first_try] Successfully launch flutter app

---
+++ Testing/appium/first_try/flutter_test.js
@@ -0,0 +1,30 @@
+// https://appium.github.io/appium/docs/en/2.0/quickstart/test-js/
+const {remote} = require('webdriverio');
+
+const capabilities = {
+  platformName: 'Android',
+  'appium:automationName': 'UiAutomator2',
+  'appium:deviceName': 'Android',
+  'appium:appPackage': 'com.example.study_flutter_integration_test',
+  'appium:appActivity': 'com.example.study_flutter_integration_test.MainActivity',
+};
+
+const wdOpts = {
+  host: process.env.APPIUM_HOST || 'localhost',
+  port: parseInt(process.env.APPIUM_PORT, 10) || 4723,
+  logLevel: 'info',
+  capabilities,
+};
+
+async function runTest() {
+  const driver = await remote(wdOpts);
+  try {
+    const batteryItem = await driver.$('//*[@text="Battery"]');
+    await batteryItem.click();
+  } finally {
+    await driver.pause(1000);
+    await driver.deleteSession();
+  }
+}
+
+runTest().catch(console.error);

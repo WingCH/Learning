12d2e160345a425f3783f3c94c0a8b20a40908ca
Wing Chan <wing.chan@keysocapp.com>
2022-09-30 18:11:12 +0800

    [Testing][appium][first_try] add `waitFor` example

--- Testing/appium/first_try/flutter_test.js
+++ Testing/appium/first_try/flutter_test.js
@@ -1,7 +1,7 @@
 // https://appium.github.io/appium/docs/en/2.0/quickstart/test-js/
-const { remote } = require('webdriverio');
+const {remote} = require('webdriverio');
 const assert = require('assert');
-const { byValueKey } = require('appium-flutter-finder');
+const {byValueKey} = require('appium-flutter-finder');
 
 // capabilities : https://appium.io/docs/en/writing-running-appium/caps/index.html
 /*
@@ -23,37 +23,41 @@ Appium Inspector does not support 'appium:automationName': 'Flutter', so use `Ui
 // };
 
 const capabilities = {
-  'appium:platformName': 'iOS',
-  'appium:automationName': 'Flutter',
-  'appium:deviceName': 'iPhone 13 Pro',
-  'appium:bundleId': 'com.example.studyFlutterIntegrationTest',
+    'appium:platformName': 'iOS',
+    'appium:automationName': 'Flutter',
+    'appium:deviceName': 'iPhone 13 Pro',
+    'appium:bundleId': 'com.example.studyFlutterIntegrationTest',
 };
 
 const wdOpts = {
-  host: process.env.APPIUM_HOST || 'localhost',
-  port: parseInt(process.env.APPIUM_PORT, 10) || 4723,
-  logLevel: 'info',
-  capabilities,
+    host: process.env.APPIUM_HOST || 'localhost',
+    port: parseInt(process.env.APPIUM_PORT, 10) || 4723,
+    logLevel: 'info',
+    capabilities,
 };
 
 async function runTest() {
-  const counterTextFinder = byValueKey('counter');
-  const buttonFinder = byValueKey('increment');
+    const counterTextFinder = byValueKey('counter');
+    const buttonFinder = byValueKey('increment');
 
-  const driver = await remote(wdOpts);
-  try {
-    assert.strictEqual(await driver.getElementText(counterTextFinder), '0');
-    await driver.elementClick(buttonFinder);
-    await driver.touchAction({
-      action: 'tap',
-      element: { elementId: buttonFinder }
-    });
+    const driver = await remote(wdOpts);
 
-    assert.strictEqual(await driver.getElementText(counterTextFinder), '2');
-  } finally {
-    await driver.pause(1000);
-    await driver.deleteSession();
-  }
+    try {
+        // this will throw error if not found counterText after 1 second
+        const result = await driver.execute('flutter:waitFor', counterTextFinder, 1)
+
+        assert.strictEqual(await driver.getElementText(counterTextFinder), '0');
+        await driver.elementClick(buttonFinder);
+        await driver.touchAction({
+            action: 'tap',
+            element: {elementId: buttonFinder}
+        });
+
+        assert.strictEqual(await driver.getElementText(counterTextFinder), '2');
+    } finally {
+        await driver.pause(1000);
+        await driver.deleteSession();
+    }
 }
 
 runTest().catch(console.error);

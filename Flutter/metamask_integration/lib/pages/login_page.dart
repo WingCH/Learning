43c714bf4f19697f027aa15034a2379579b47873
Wing Chan <wing.chan@keysocapp.com>
2022-10-03 10:33:24 +0800

    [Flutter][metamask_integration] connect Metamask

--- Flutter/metamask_integration/lib/pages/login_page.dart
+++ Flutter/metamask_integration/lib/pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:walletconnect_dart/walletconnect_dart.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -8,6 +10,40 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  var connector = WalletConnect(
+    bridge: 'https://bridge.walletconnect.org',
+    clientMeta: const PeerMeta(
+      name: 'My App',
+      description: 'An app for converting pictures to NFT',
+      url: 'https://walletconnect.org',
+      icons: [
+        'https://files.gitbook.com/v0/b/gitbook-legacy-files/o/spaces%2F-LJJeCjcLrr53DcT1Ml7%2Favatar.png?alt=media'
+      ],
+    ),
+  );
+
+  SessionStatus? _session;
+  String? _uri;
+
+  void loginUsingMetamask(BuildContext context) async {
+    if (!connector.connected) {
+      try {
+        var session = await connector.createSession(onDisplayUri: (uri) async {
+          // e.g: wc:6fabddfa-5353-4011-8f08-f458f4f877bf@1?bridge=https%3A%2F%2Ft.bridge.walletconnect.org&key=a00825511cb421c5e6c9f4a4f9142797a0667a96516a82293a2adf7513a5b441
+          _uri = uri;
+          await launchUrlString(uri, mode: LaunchMode.externalApplication);
+        });
+        print(session.accounts[0]);
+        print(session.chainId);
+        setState(() {
+          _session = session;
+        });
+      } catch (exp) {
+        print(exp);
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +59,9 @@ class _LoginPageState extends State<LoginPage> {
               fit: BoxFit.fitHeight,
             ),
             ElevatedButton(
-                onPressed: () => {}, child: const Text("Connect with Metamask"))
+              onPressed: () => loginUsingMetamask(context),
+              child: const Text("Connect with Metamask"),
+            )
           ],
         ),
       ),

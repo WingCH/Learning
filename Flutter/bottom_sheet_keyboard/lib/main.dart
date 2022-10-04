f6b3c0285e9afcdcb6b855e7a3f6e6a090366034
Wing CHAN <wingch.apps@gmail.com>
2022-10-04 11:50:07 +0800

    [Flutter][bottom_sheet_keyboard] update

--- Flutter/bottom_sheet_keyboard/lib/main.dart
+++ Flutter/bottom_sheet_keyboard/lib/main.dart
@@ -1,28 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(),
@@ -52,8 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     context: context,
                     builder: (BuildContext context) {
                       return Padding(
-                        padding: EdgeInsets.only(
-                            bottom: MediaQuery.of(context).viewInsets.bottom),
+                        padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
                         child: Column(
                           mainAxisSize: MainAxisSize.min,
                           children: const [
@@ -65,28 +56,6 @@ class _MyHomePageState extends State<MyHomePage> {
                     });
               },
               child: const Text('Open Bottom Sheet'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                showBarModalBottomSheet(
-                  context: context,
-                  expand: false,
-                  builder: (BuildContext context) {
-                    return Padding(
-                      padding: EdgeInsets.only(
-                          bottom: MediaQuery.of(context).viewInsets.bottom),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: const [
-                          Text('This is a modal'),
-                          TextField(autofocus: true),
-                        ],
-                      ),
-                    );
-                  },
-                );
-              },
-              child: const Text('Open Bottom Sheet (modal_bottom_sheet)'),
             ),
           ],
         ),

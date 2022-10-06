3a9a13c0c314dd2080ecbefd0c25ecb9f102e0bf
Wing Chan <wing.chan@keysocapp.com>
2022-10-06 13:27:50 +0800

    [Flutter][study_from] add comment

--- Flutter/study_form/lib/main.dart
+++ Flutter/study_form/lib/main.dart
@@ -27,10 +27,69 @@ class Form1Page extends StatefulWidget {
 }
 
 class _Form1PageState extends State<Form1Page> {
+  final _emailFieldKey = GlobalKey<FormFieldState>();
+
+  /*
+  Issue:
+  1. Cannot dynamically change error text, such as need call api to check email is valid or not.
+  2. Cannot control error widget position, such as need show error text below the input field.
+   */
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Center(
+        child: SizedBox(
+          width: 200,
+          height: 250,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ColoredBox(
+                color: Colors.transparent,
+                child: TextFormField(
+                  // error
+                  key: _emailFieldKey,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    label: ColoredBox(
+                      color: Colors.yellow,
+                      child: Text('Enter your email'),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  _emailFieldKey.currentState?.reset();
+                },
+                child: const Text('Reset'),
+              ),
+              TextButton(
+                onPressed: () {
+                  _emailFieldKey.currentState?.validate();
+                },
+                child: const Text('validate'),
+              ),
+              TextButton(
+                onPressed: () {
+                  _emailFieldKey.currentState?.save();
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content:
+                        Text('Saved ${_emailFieldKey.currentState?.value}'),
+                  ));
+                },
+                child: const Text('Saved'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }

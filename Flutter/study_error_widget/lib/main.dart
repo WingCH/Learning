0308a1073b7416ffa85ba4245939ead23463ffbe
Wing Chan <wing.chan@keysocapp.com>
2022-10-07 11:43:14 +0800

    [flutter][study_error_widget] add comment

--- Flutter/study_error_widget/lib/main.dart
+++ Flutter/study_error_widget/lib/main.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
 
+// ref: https://juejin.cn/post/7134510980905566238
+
+// clone by ErrorWidget.builder -> _defaultErrorWidgetBuilder
+Widget _defaultErrorWidgetBuilder(FlutterErrorDetails details) {
+  String message =
+      '${details.exception}\nSee also: https://flutter.dev/docs/testing/errors';
+
+  // In production code, assertions are ignored, and the arguments to assert aren’t evaluated.
+  // https://dart.dev/guides/language/language-tour#assert
+  // assert(() {
+  //   message = '${_stringify(details.exception)}\nSee also: https://flutter.dev/docs/testing/errors';
+  //   return true;
+  // }());
+
+  print('_defaultErrorWidgetBuilder');
+  final Object exception = details.exception;
+  return ErrorWidget.withDetails(
+      message: message, error: exception is FlutterError ? exception : null);
+}
+
 void main() {
+  ErrorWidget.builder = _defaultErrorWidgetBuilder;
   runApp(const MyApp());
 }
 
@@ -32,7 +53,19 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(),
+      body: Stack(
+        children: <Widget>[
+          GestureDetector(
+            child: Positioned(
+              child: Container(
+                height: 250,
+                width: 250,
+                color: Colors.red,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }

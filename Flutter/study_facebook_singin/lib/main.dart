0a7f9de1e292eb072e5608b6c5da9f657500757d
Wing Chan <wing.chan@keysocapp.com>
2022-10-10 16:58:48 +0800

    [flutter][study_facebook_singin] setup demo, android tested

--- Flutter/study_facebook_singin/lib/main.dart
+++ Flutter/study_facebook_singin/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,14 +29,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  LoginStatus? _loginStatus;
+  Map<String, dynamic>? _userData;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: const Center(
-        child: Text('HomePage'),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextButton(
+              child: const Text('Login with Facebook'),
+              onPressed: () async {
+                final LoginResult result = await FacebookAuth.instance.login();
+                setState(() {
+                  _loginStatus = result.status;
+                });
+                if (result.status == LoginStatus.success) {
+                  // you are logged
+                  final AccessToken accessToken = result.accessToken!;
+
+                  final userData = await FacebookAuth.instance.getUserData(
+                    fields:
+                        "name,email,picture.width(200),birthday,friends,gender,link",
+                  );
+                  setState(() {
+                    _userData = userData;
+                  });
+                }
+              },
+            ),
+            Text('LoginStatus', style: Theme.of(context).textTheme.headline6),
+            Text(_loginStatus.toString()),
+            const SizedBox(height: 16),
+            Text('UserData', style: Theme.of(context).textTheme.headline6),
+            Text(_userData.toString()),
+          ],
+        ),
       ),
     );
   }

bb93f1916ad3b21b61009f13e7def033e431ace3
Wing Chan <wing.chan@keysocapp.com>
2022-11-11 13:11:43 +0800

    [Rust][first_try] init

---
+++ Rust/first_try/src/main.rs
@@ -0,0 +1,10 @@
+// https://askeing.github.io/rust-book/getting-started.html
+fn main() {
+
+    /*
+    two ways to execute the function
+    1. cargo build && ./target/debug/hello_world
+    2. cargo run
+     */
+    println!("Hello world!");
+}

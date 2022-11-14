795c6eea9a5667287974637be92fb8b2e4cb6548
Wing Chan <wing.chan@keysocapp.com>
2022-11-14 09:58:04 +0800

    [Rust][first_try]

--- Rust/first_try/src/main.rs
+++ Rust/first_try/src/main.rs
@@ -1,10 +1,17 @@
 // https://askeing.github.io/rust-book/getting-started.html
+/*
+two ways to execute the function
+1. cargo build && ./target/debug/hello_world
+2. cargo run
+ */
 fn main() {
+    let a = true;
 
-    /*
-    two ways to execute the function
-    1. cargo build && ./target/debug/hello_world
-    2. cargo run
-     */
-    println!("Hello world!");
+    let _y = change_truth(a);
+    println!("{}", a);
+    println!("{}", _y);
+}
+
+fn change_truth(x: bool) -> bool {
+    !x
 }

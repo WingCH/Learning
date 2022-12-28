4baf5b124463952ca8d98a703a22a93fa924a2e1
Wing Chan <wing.chan@keysocapp.com>
2022-12-28 17:20:18 +0800

    [iOS][Animation] init project and remove Storyboard

---
+++ iOS/Animation/Animation/Animation1ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Animation1ViewController.swift
+//  Animation
+//
+//  Created by Wing CHAN on 28/12/2022.
+//
+
+import UIKit
+
+class Animation1ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+

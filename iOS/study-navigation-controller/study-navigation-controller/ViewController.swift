ccf9fb6ed6887310f4ea4ef855bc73c1f1ff04dc
WingCHAN <wingch.apps@gmail.com>
2023-06-01 20:37:28 +0800

    [study-navigation-controller] init project and remove storyboard

---
+++ iOS/study-navigation-controller/study-navigation-controller/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  study-navigation-controller
+//
+//  Created by Wing on 1/6/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+}

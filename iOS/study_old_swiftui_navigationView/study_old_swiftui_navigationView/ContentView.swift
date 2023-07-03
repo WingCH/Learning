115a770a01077f3fad3d951dc56f71e73a48da15
Wing Chan <wing.chan@keysocapp.com>
2023-07-03 18:45:59 +0800

    [study_old_swiftui_navigationView]

--- iOS/study_old_swiftui_navigationView/study_old_swiftui_navigationView/ContentView.swift
+++ iOS/study_old_swiftui_navigationView/study_old_swiftui_navigationView/ContentView.swift
@@ -9,52 +9,77 @@ import SwiftUI
 
 struct SheetView: View {
     @Binding var showSheetView: Bool
+    @State private var selection: String? = nil
+    @Environment(\.dismiss) var dismiss
+
     var body: some View {
         NavigationView {
-            Text("List of notifications")
-                .navigationBarTitle(Text("Notifications"), displayMode: .inline)
-                .navigationBarItems(trailing: Button(action: {
-                    print("Dismissing sheet view...")
-                    self.showSheetView = false
-                }) {
-                    Text("Done").bold()
-                })
+            VStack {
+                NavigationLink(destination: Text("Third View"), tag: "Third", selection: $selection) { EmptyView() }
+                Button("Tap to show third") {
+                    self.selection = "Third"
+                }
+                Button("Dismiss") {
+                    dismiss()
+                }
+            }
         }
+    }
+}
+
+struct ChildView: View {
+    var body: some View {
+        Text("You cannot swipe back")
+            // navigationBarBackButtonHidden will block swipe back gesture
+            // search `UIGestureRecognizerDelegate` to fix this issue
+            .navigationBarBackButtonHidden(true)
     }
 }
 
 struct ContentView: View {
     @State var showSheetView = false
     @State private var selection: String? = nil
+    @Environment(\.presentationMode) var presentationMode
 
     var body: some View {
         NavigationView {
             VStack {
-                NavigationLink(destination: Text("Second View"), tag: "Second", selection: $selection) { EmptyView() }
-                NavigationLink(destination: Text("Third View"), tag: "Third", selection: $selection) { EmptyView() }
+                NavigationLink(destination: ChildView(),
+                               tag: "Second", selection: $selection) { EmptyView() }
+
                 Button("Tap to show second") {
                     self.selection = "Second"
                 }
-                Button("Tap to show third") {
-                    self.selection = "Third"
-                }
+
                 Button("Show Full Screen Cover") {
                     self.showSheetView = true
                 }
             }
             .navigationTitle("Navigation")
         }.fullScreenCover(
-            isPresented: $showSheetView,
-            onDismiss: {
-                self.selection = "Second"
-            }) {
-                SheetView(showSheetView: $showSheetView)
-            }
+            isPresented: $showSheetView
+        ) {
+            SheetView(showSheetView: $showSheetView)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+// navigationBarBackButtonHidden will block swipe back gesture
+// use .navigationBarBackButtonHidden(true),  but keep swipe back gesture
+// https://stackoverflow.com/a/60067845/5588637
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
     }
 }

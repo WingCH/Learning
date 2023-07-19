cf6242aa5f5cbd00256442e2ccb625cf5c604521
Wing Chan <wing.chan@keysocapp.com>
2023-07-19 10:41:11 +0800

    [study_uikit_to_image]

---
+++ iOS/study_uikit_to_image/study_uikit_to_image/ViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ViewController.swift
+//  study_uikit_to_image
+//
+//  Created by Wing CHAN on 19/7/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let colorsCount = 460
+    var colors = [UIColor]()
+
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    lazy var button: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setTitle("Save to image", for: .normal)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.addTarget(self, action: #selector(saveToImage), for: .touchUpInside)
+        return btn
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupColors()
+        setupViews()
+    }
+
+    private func setupColors() {
+        for _ in 0 ..< colorsCount {
+            colors.append(UIColor(red: CGFloat(drand48()), green: CGFloat(drand48()), blue: CGFloat(drand48()), alpha: 1.0))
+        }
+    }
+
+    private func setupViews() {
+        view.addSubview(scrollView)
+        view.addSubview(button)
+        scrollView.addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: button.topAnchor),
+
+            button.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            button.heightAnchor.constraint(equalToConstant: 50),
+
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+
+        for (index, color) in colors.enumerated() {
+            let label = UILabel()
+            label.text = "Hello, world! \(index)"
+            label.textColor = .white
+            label.textAlignment = .center
+            label.backgroundColor = color
+            stackView.addArrangedSubview(label)
+        }
+    }
+
+    @objc func saveToImage() {
+        let image = stackView.asImage()
+        print(image)
+    }
+}
+
+extension UIView {
+    func asImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
+    }
+}

29e04435774d99c1a68ee5fbe2fc6b9a298cfd80
Wing Chan <wing.chan@keysocapp.com>
2023-07-20 15:07:15 +0800

    [study_swiftui_fullScreenCover]

---
+++ iOS/study_swiftui_fullScreenCover/study_swiftui_fullScreenCover/study_swiftui_fullScreenCoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  study_swiftui_fullScreenCoverApp.swift
+//  study_swiftui_fullScreenCover
+//
+//  Created by Wing CHAN on 20/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct study_swiftui_fullScreenCoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

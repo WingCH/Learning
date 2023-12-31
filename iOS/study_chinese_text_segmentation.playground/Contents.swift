cc1dad780b221e03c7163fef2f6de99eabaa71cb
WingCHAN <wingch.apps@gmail.com>
2023-12-31 19:48:00 +0800

    study_chinese_text_segmentation

--- iOS/study_chinese_text_segmentation.playground/Contents.swift
+++ iOS/study_chinese_text_segmentation.playground/Contents.swift
@@ -1,20 +1,20 @@
 import NaturalLanguage
 
 //  同NLTokenizer(unit: .word) 效果一樣
-//func tokenize(text: String) -> [String] {
-//    let tokenize = CFStringTokenizerCreate(kCFAllocatorDefault, text as CFString?, CFRangeMake(0, text.count), kCFStringTokenizerUnitWord, CFLocaleCopyCurrent())
-//    CFStringTokenizerAdvanceToNextToken(tokenize)
-//    var range = CFStringTokenizerGetCurrentTokenRange(tokenize)
-//    var keyWords: [String] = []
-//    while range.length > 0 {
-//        let wRange = text.index(text.startIndex, offsetBy: range.location) ..< text.index(text.startIndex, offsetBy: range.location + range.length)
-//        let keyWord = String(text[wRange])
-//        keyWords.append(keyWord)
-//        CFStringTokenizerAdvanceToNextToken(tokenize)
-//        range = CFStringTokenizerGetCurrentTokenRange(tokenize)
-//    }
-//    return keyWords
-//}
+func tokenize(text: String) -> [String] {
+    let tokenize = CFStringTokenizerCreate(kCFAllocatorDefault, text as CFString?, CFRangeMake(0, text.count), kCFStringTokenizerUnitWord, CFLocaleCopyCurrent())
+    CFStringTokenizerAdvanceToNextToken(tokenize)
+    var range = CFStringTokenizerGetCurrentTokenRange(tokenize)
+    var keyWords: [String] = []
+    while range.length > 0 {
+        let wRange = text.index(text.startIndex, offsetBy: range.location) ..< text.index(text.startIndex, offsetBy: range.location + range.length)
+        let keyWord = String(text[wRange])
+        keyWords.append(keyWord)
+        CFStringTokenizerAdvanceToNextToken(tokenize)
+        range = CFStringTokenizerGetCurrentTokenRange(tokenize)
+    }
+    return keyWords
+}
 
 
 func tokenize2(text: String) -> [String] {
@@ -30,11 +30,16 @@ func tokenize2(text: String) -> [String] {
     return keyWords
 }
 
-let text = """
-港女食 Five Guys 叫芝士漢堡包走包 結果冬天嘅瑞士係咪無咁值唔值得去
+var text = """
+19分鐘前 •查看翻譯
 """
-//let words = tokenize(text: text)
+/*
+ 要實現在分詞前刪除所有符號，只保留空格、文字和數字，你可以在 tokenize 函數中加入一個步驟來處理這個需求。以下是修改後的 tokenize 函數，它首先使用正則表達式移除非文字、非數字和非空格的字符，然後再進行分詞：
+ */
+text = text.replacingOccurrences(of: "[^\\w\\s]", with: "", options: .regularExpression)
+
+let words = tokenize(text: text)
 let words2 = tokenize2(text: text)
 
-//print(words)
+print(words)
 print(words2)

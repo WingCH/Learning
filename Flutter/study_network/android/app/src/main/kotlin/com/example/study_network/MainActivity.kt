6e4c954eda8c8ff959877ae2237b662f4e30ed9d
WingCHAN <wingch.apps@gmail.com>
2024-02-04 15:55:03 +0800

    [study_network]

---
+++ Flutter/study_network/android/app/src/main/kotlin/com/example/study_network/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.study_network
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/study_network/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'http_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text("http"),
+            trailing: const Icon(Icons.arrow_forward),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HttpPage(),
+                ),
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}

f9e3136a21afc389888f5d0cff8822cd285de122
Wing Chan <wing.chan@keysocapp.com>
2024-04-12 18:28:08 +0800

    [study_drawer] UIViewControllerTransitioningDelegate -> UIPresentationController

--- iOS/study_drawer/study_drawer/ViewController.swift
+++ iOS/study_drawer/study_drawer/ViewController.swift
@@ -22,8 +22,16 @@ class ViewController: UIViewController {
         let button1 = UIButton(type: .system)
         button1.setTitle("UISheetPresentationController", for: .normal)
         
+        let label2 = UILabel()
+        label2.numberOfLines = 0
+        label2.text = """
+        UISheetPresentationController
+        Issue:
+        1. cannot disable `presentingViewController` animation when detents is large
+        2. Custom Height need iOS 16
+        """
         let button2 = UIButton(type: .system)
-        button2.setTitle("Button 2", for: .normal)
+        button2.setTitle("UIViewControllerTransitioningDelegate -> UIPresentationController", for: .normal)
         
         let button3 = UIButton(type: .system)
         button3.setTitle("Button 3", for: .normal)
@@ -33,6 +41,7 @@ class ViewController: UIViewController {
             arrangedSubviews: [
                 label1,
                 button1,
+                label2,
                 button2,
                 button3
             ]
@@ -63,10 +72,82 @@ class ViewController: UIViewController {
     }
     
     @objc func button2Tapped() {
-        print("Button 2 was tapped.")
+        let vc = ViewController2()
+        vc.modalPresentationStyle = .custom
+        vc.transitioningDelegate = self
+        present(vc, animated: true, completion: nil)
     }
     
     @objc func button3Tapped() {
         print("Button 3 was tapped.")
+    }
+}
+
+extension ViewController: UIViewControllerTransitioningDelegate {
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        PresentationController(presentedViewController: presented, presenting: presenting)
+    }
+}
+
+// https://stackoverflow.com/a/70360369
+class PresentationController: UIPresentationController {
+    let blurEffectView: UIVisualEffectView!
+    var tapGestureRecognizer: UITapGestureRecognizer = UITapGestureRecognizer()
+  
+    override init(presentedViewController: UIViewController, presenting presentingViewController: UIViewController?) {
+        let blurEffect = UIBlurEffect(style: .dark)
+        blurEffectView = UIVisualEffectView(effect: blurEffect)
+        super.init(presentedViewController: presentedViewController, presenting: presentingViewController)
+        tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissController))
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.blurEffectView.isUserInteractionEnabled = true
+        self.blurEffectView.addGestureRecognizer(tapGestureRecognizer)
+    }
+  
+    override var frameOfPresentedViewInContainerView: CGRect {
+        CGRect(origin: CGPoint(x: 0, y: self.containerView!.frame.height * 0.4),
+               size: CGSize(width: self.containerView!.frame.width, height: self.containerView!.frame.height *
+                   0.6))
+    }
+
+    override func presentationTransitionWillBegin() {
+        self.blurEffectView.alpha = 0
+        self.containerView?.addSubview(blurEffectView)
+        self.presentedViewController.transitionCoordinator?.animate(alongsideTransition: { _ in
+            self.blurEffectView.alpha = 0.7
+        }, completion: { _ in })
+    }
+  
+    override func dismissalTransitionWillBegin() {
+        self.presentedViewController.transitionCoordinator?.animate(alongsideTransition: { _ in
+            self.blurEffectView.alpha = 0
+        }, completion: { _ in
+            self.blurEffectView.removeFromSuperview()
+        })
+    }
+  
+    override func containerViewWillLayoutSubviews() {
+        super.containerViewWillLayoutSubviews()
+        presentedView!.roundCorners([.topLeft, .topRight], radius: 22)
+    }
+
+    override func containerViewDidLayoutSubviews() {
+        super.containerViewDidLayoutSubviews()
+        presentedView?.frame = frameOfPresentedViewInContainerView
+        blurEffectView.frame = containerView!.bounds
+    }
+
+    @objc func dismissController() {
+        self.presentedViewController.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension UIView {
+    func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners,
+                                cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
     }
 }

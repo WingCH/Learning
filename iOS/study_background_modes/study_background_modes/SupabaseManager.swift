240ff137e1228596fccf09aef84ab65bafed95eb
WingCHAN <wingch.apps@gmail.com>
2024-05-04 18:12:52 +0800

    [study_background_modes] add user id and memory_usage_in_mb

--- iOS/study_background_modes/study_background_modes/SupabaseManager.swift
+++ iOS/study_background_modes/study_background_modes/SupabaseManager.swift
@@ -13,6 +13,16 @@ import Supabase
 class SupabaseManager {
     private let logger = Logger()
     static let shared = SupabaseManager()
+    var user: User?
+
+    init() {
+        Task {
+            for await authStateChange in authStateChanges {
+                self.user = authStateChange.session?.user
+            }
+        }
+    }
+
     private let supabase = SupabaseClient(
         supabaseURL: URL(string: "https://ogqbuqxexajleyblwmlp.supabase.co")!,
         supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ncWJ1cXhleGFqbGV5Ymx3bWxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ2NTg4NjQsImV4cCI6MjAzMDIzNDg2NH0.2tBM90LuF5RTtitXyEN-PzrS1JNtJO6Erw8ehFY-Pqc"
@@ -41,6 +51,9 @@ class SupabaseManager {
     }
 
     func insertData(logData: LogData) async {
+        var logData = logData
+        logData.extra["memory_usage_in_mb"] = String(describing: formattedMemoryFootprint())
+        logData.extra["user"] = user?.id.queryValue ?? "null"
         do {
             try await supabase.from("BackgroundMode").insert(logData).execute()
         } catch {
@@ -74,5 +87,33 @@ extension SupabaseManager {
             extra: \(extra)
             """
         }
+    }
+
+    // https://stackoverflow.com/a/64893753/5588637
+    func memoryFootprint() -> Float? {
+        // The `TASK_VM_INFO_COUNT` and `TASK_VM_INFO_REV1_COUNT` macros are too
+        // complex for the Swift C importer, so we have to define them ourselves.
+        let TASK_VM_INFO_COUNT = mach_msg_type_number_t(MemoryLayout<task_vm_info_data_t>.size / MemoryLayout<integer_t>.size)
+        let TASK_VM_INFO_REV1_COUNT = mach_msg_type_number_t(MemoryLayout.offset(of: \task_vm_info_data_t.min_address)! / MemoryLayout<integer_t>.size)
+        var info = task_vm_info_data_t()
+        var count = TASK_VM_INFO_COUNT
+        let kr = withUnsafeMutablePointer(to: &info) { infoPtr in
+            infoPtr.withMemoryRebound(to: integer_t.self, capacity: Int(count)) { intPtr in
+                task_info(mach_task_self_, task_flavor_t(TASK_VM_INFO), intPtr, &count)
+            }
+        }
+        guard
+            kr == KERN_SUCCESS,
+            count >= TASK_VM_INFO_REV1_COUNT
+        else { return nil }
+
+        let usedBytes = Float(info.phys_footprint)
+        return usedBytes
+    }
+
+    func formattedMemoryFootprint() -> String {
+        let usedBytes: UInt64? = UInt64(self.memoryFootprint() ?? 0)
+        let usedMB = Double(usedBytes ?? 0) / 1024 / 1024
+        return String(describing: usedMB)
     }
 }

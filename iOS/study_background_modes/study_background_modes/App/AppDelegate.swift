4478ab7af516b39cb38dba509eab24fcced67350
WingCHAN <wingch.apps@gmail.com>
2024-05-11 18:45:15 +0800

    [study_flutter_engine]

--- iOS/study_background_modes/study_background_modes/App/AppDelegate.swift
+++ iOS/study_background_modes/study_background_modes/App/AppDelegate.swift
@@ -160,4 +160,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             )
         }
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return true
+    }
 }

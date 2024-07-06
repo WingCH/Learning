232da94f645f84f4b6753cb3a5532e0a07a1c519
WingCHAN <wingch.apps@gmail.com>
2024-07-06 10:31:47 +0800

    [study_stick_bottom_button_with_keyboard]

--- Flutter/study_stick_bottom_button_with_keyboard/lib/main.dart
+++ Flutter/study_stick_bottom_button_with_keyboard/lib/main.dart
@@ -25,72 +25,48 @@ class BottomScrollViewExample extends StatefulWidget {
 }
 
 class _BottomScrollViewExampleState extends State<BottomScrollViewExample> {
-  List<FocusNode> _focusNodes = [];
-  ScrollController _scrollController = new ScrollController();
-
-  @override
-  void initState() {
-    super.initState();
-    for (int i = 0; i < 20; i++) {
-      FocusNode focusNode = FocusNode();
-      focusNode.addListener(() {
-        if (focusNode.hasFocus) {
-          // delay to ensure the keyboard is shown
-          Future.delayed(const Duration(milliseconds: 250), () {
-            _scrollController.animateTo(
-              focusNode.context?.findRenderObject()?.getTransformTo(null).getTranslation().y ?? 0,
-              duration: const Duration(milliseconds: 250),
-              curve: Curves.linear,
-            );
-          });
-        }
-      });
-      _focusNodes.add(focusNode);
-    }
-  }
-
-  @override
-  void dispose() {
-    for (FocusNode focusNode in _focusNodes) {
-      focusNode.dispose();
-    }
-    super.dispose();
-  }
+  final bottomHeight = 100.0;
 
   @override
   Widget build(BuildContext context) {
+    final keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Column(
-        children: <Widget>[
-          Expanded(
+      body: Stack(
+        children: [
+          Positioned.fill(
             child: SingleChildScrollView(
-              controller: _scrollController,
-              padding: EdgeInsets.only(bottom: max(MediaQuery.of(context).viewInsets.bottom - 100, 0)),
               child: Column(
                 children: <Widget>[
-                  for (int i = 0; i < _focusNodes.length; i++)
+                  for (int i = 0; i < 20; i++)
                     TextField(
-                      focusNode: _focusNodes[i],
+                      scrollPadding: EdgeInsets.only(bottom: keyboardHeight),
                       decoration: InputDecoration(
                         labelText: 'Text Field $i',
                       ),
                     ),
+                  SizedBox(
+                    height: max(keyboardHeight, bottomHeight),
+                  ),
                 ],
               ),
             ),
           ),
-          SafeArea(
-            top: false,
-            bottom: false,
-            child: Container(
-              color: Colors.blue,
-              height: 100.0,
-              padding: const EdgeInsets.all(16.0),
-              child: const Center(
-                child: Text(
-                  'Bottom Container',
-                  style: TextStyle(color: Colors.white),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: SafeArea(
+              bottom: false,
+              child: Container(
+                color: Colors.blue,
+                height: bottomHeight,
+                padding: const EdgeInsets.all(16.0),
+                child: const Center(
+                  child: Text(
+                    'Bottom Container',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
               ),
             ),

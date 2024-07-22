bc1cf0eecf87f61eaecf3c9403a5fe11f50389e5
WingCHAN <wingch.apps@gmail.com>
2024-07-22 19:18:51 +0800

    [feat] - Update background channel method and UI improvements -  Updated the `invokeMethod` call in `MainViewController.swift` to pass arguments. -  Refactored method call handling in `main.dart` to include a specific check for 'waitResult' method. -  Enhanced the `MyHomePage` widget to display the battery level or error message on the button. -  Improved code readability by reformatting long lines.

--- Flutter/study_ios_manual_run_engine/lib/main.dart
+++ Flutter/study_ios_manual_run_engine/lib/main.dart
@@ -4,8 +4,10 @@ import 'package:battery_plus/battery_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-const foregroundChannel = MethodChannel('samples.flutter.dev/foregroundChannel');
-const backgroundChannel = MethodChannel("samples.flutter.dev/backgroundChannel");
+const foregroundChannel =
+    MethodChannel('samples.flutter.dev/foregroundChannel');
+const backgroundChannel =
+    MethodChannel("samples.flutter.dev/backgroundChannel");
 
 @pragma('vm:entry-point')
 void customEntryPoint() {
@@ -14,19 +16,20 @@ void customEntryPoint() {
   backgroundChannel.setMethodCallHandler((call) async {
     final method = call.method;
     final args = call.arguments;
-    print('Received call from background: ${method}, Args: ${args}');
-    print('Sleeping for 1 seconds..., Date: ${DateTime.now()}');
-    await Future.delayed(const Duration(seconds: 1));
-    try {
-      // Demo use other flutter plugin in background isolate
-      var battery = Battery();
-      final batteryLevel = await battery.batteryLevel;
-      return batteryLevel;
-    } catch (e) {
-      return e.toString();
+    print('Received call from background: $method, Args: $args');
+    if (method == 'waitResult') {
+      print('Sleeping for 1 seconds..., Date: ${DateTime.now()}');
+      await Future.delayed(const Duration(seconds: 1));
+      try {
+        // Demo use other flutter plugin in background isolate
+        var battery = Battery();
+        final batteryLevel = await battery.batteryLevel;
+        return batteryLevel;
+      } catch (e) {
+        return e.toString();
+      }
     }
   });
-  print('Hello from custom entry point!');
 }
 
 void main() {
@@ -60,11 +63,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String result = 'initial';
+
   @override
   void initState() {
     super.initState();
-    var battery = Battery();
-    battery.batteryLevel.then((value) => print('Battery level: ${value}'));
+    getBatteryLevel();
+  }
+
+  void getBatteryLevel() async {
+    try {
+      var battery = Battery();
+      final batteryLevel = await battery.batteryLevel;
+      result = 'Battery level: $batteryLevel';
+    } catch (e) {
+      result = e.toString();
+    }
+    setState(() {});
   }
 
   @override
@@ -82,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
               onPressed: () {
                 initial(customEntryPoint);
               },
-              child: const Text('initial'),
+              child: Text(result),
             ),
           ],
         ),
@@ -93,7 +108,8 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> initial(
     final Function callbackDispatcher,
   ) async {
-    CallbackHandle? callbackHandle = PluginUtilities.getCallbackHandle(callbackDispatcher);
+    CallbackHandle? callbackHandle =
+        PluginUtilities.getCallbackHandle(callbackDispatcher);
     if (callbackHandle == null) {
       print('Failed to get callback handle');
       return;

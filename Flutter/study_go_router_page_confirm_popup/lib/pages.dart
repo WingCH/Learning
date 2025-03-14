2a64258d5fed386e1e1557e4b5af572bfe3f26c3
WingCHAN <wingch.apps@gmail.com>
2025-03-14 22:09:51 +0800

    Refactor PageA to use GestureDetector for navigation confirmation on iOS, replacing PopScope implementation.

--- Flutter/study_go_router_page_confirm_popup/lib/pages.dart
+++ Flutter/study_go_router_page_confirm_popup/lib/pages.dart
@@ -31,36 +31,54 @@ class PageA extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      canPop: false,
-      onPopInvokedWithResult: (bool didPop, Object? result) {
-        debugPrint('onPopInvokedWithResult: $didPop, $result');
-        if (didPop) {
-          return;
+    // TODO: Only for iOS is ok, android can ignore this
+    return GestureDetector(
+      onHorizontalDragUpdate: (details) {
+        //set the sensitivity for your ios gesture anywhere between 10-50 is good
+        int sensitivity = 20;
+        // print(details.delta.dx);
+
+        if (details.delta.dx > sensitivity) {
+          context.showNavigationConfirmDialog(
+            title: '確認返回',
+            content: '你確定要離開當前頁面嗎？\nTrigger by PopScope.onPopInvokedWithResult',
+            onConfirm: () => Navigator.pop(
+                context), // don't use context.pop(), because it will trigger GoRouter.pop
+          );
         }
-        context.showNavigationConfirmDialog(
-          title: '確認返回',
-          content: '你確定要離開當前頁面嗎？\nTrigger by PopScope.onPopInvokedWithResult',
-          onConfirm: () => Navigator.pop(context), // don't use context.pop(), because it will trigger GoRouter.pop
-        );
       },
-      child: Scaffold(
-        appBar: AppBar(title: const Text('PageA Screen')),
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () => context.push('/pageB'),
-                child: const Text('Push to the PageB screen'),
-              ),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () => context.pop(),
-                child: const Text('Pop'),
-              ),
-            ],
+      child: PopScope(
+        canPop: false,
+        onPopInvokedWithResult: (bool didPop, Object? result) {
+          debugPrint('onPopInvokedWithResult: $didPop, $result');
+          if (didPop) {
+            return;
+          }
+          context.showNavigationConfirmDialog(
+            title: '確認返回',
+            content: '你確定要離開當前頁面嗎？\nTrigger by PopScope.onPopInvokedWithResult',
+            onConfirm: () => Navigator.pop(
+                context), // don't use context.pop(), because it will trigger GoRouter.pop
+          );
+        },
+        child: Scaffold(
+          appBar: AppBar(title: const Text('PageA Screen')),
+          body: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => context.push('/pageB'),
+                  child: const Text('Push to the PageB screen'),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () => context.pop(),
+                  child: const Text('Pop'),
+                ),
+              ],
+            ),
           ),
         ),
       ),

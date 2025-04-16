68334d52177f3af13719dc6ef836d19c2f289158
Wing CHAN <wing-hwchan@vgt-hk.net>
2025-04-16 16:25:06 +0800

    [study_flutter_slidable] init

---
+++ Flutter/study_flutter_slidable/README.md
@@ -0,0 +1,3 @@
+# study_flutter_slidable
+
+A new Flutter project.
---
+++ Flutter/study_flutter_slidable/android/app/src/main/kotlin/com/example/study_flutter_slidable/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.study_flutter_slidable
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()

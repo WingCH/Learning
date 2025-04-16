c2371188f75804dabfd0ed265126ae939593c290
WingCHAN <wingch.apps@gmail.com>
2025-04-16 22:42:25 +0800

    Revert "[study_gesture_arena_team]"
    This reverts commit 4de6baa8854accc943a0c6af14bb7bd91fa3bda2.

---
+++ Flutter/study_gesture_arena_team/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/wingch/fvm/versions/3.24.4"
+export "FLUTTER_APPLICATION_PATH=/Users/wingch/Project/Learning/Flutter/study_gesture_arena_team"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=0.1.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"

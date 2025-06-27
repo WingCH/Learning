cdd08b266fa9037e487a344e6a765a7de2819d0e
WingCHAN <wingch.apps@gmail.com>
2025-06-27 22:35:56 +0800

    [study_horizontal_nest_scroll] solution1

--- Flutter/study_horizontal_nest_scroll/lib/main.dart
+++ Flutter/study_horizontal_nest_scroll/lib/main.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: NestedScrollDemo()));
+void main() => runApp(const MaterialApp(home: NestedScrollDemo()));
 
-/*
-巢狀橫向滾動的手勢衝突問題
+// 自定義通知，用於內層通知外層需要滾動
+class OuterScrollNotification extends Notification {
+  final double delta;
+  OuterScrollNotification(this.delta);
+}
 
-  當你有兩層都是橫向滾動的 ListView 時：
-  - 外層：灰色容器內的橫向 ListView（顯示紅色卡片）
-  - 內層：每個紅色卡片內的橫向 ListView（顯示藍色方塊）
-
-  問題描述：
-
-  1. 手勢競爭：當用戶在藍色區域橫向滑動時，Flutter 不知道應該滾動哪一層
-  2. 預設行為：內層 ListView 會優先捕獲手勢，導致外層難以滾動
-  3. 用戶體驗差：用戶必須在非常特定的位置（紅色但非藍色區域）才能滾動外層
-*/
-class NestedScrollDemo extends StatelessWidget {
+class NestedScrollDemo extends StatefulWidget {
   const NestedScrollDemo({super.key});
+
+  @override
+  State<NestedScrollDemo> createState() => _NestedScrollDemoState();
+}
+
+class _NestedScrollDemoState extends State<NestedScrollDemo> {
+  final ScrollController _outerController = ScrollController();
+
+  @override
+  void dispose() {
+    _outerController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,23 +31,133 @@ class NestedScrollDemo extends StatelessWidget {
         child: Container(
           height: 200,
           color: Colors.grey[300],
-          child: ListView.builder(
-            itemCount: 2,
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (_, i) => Container(
-              width: 300,
-              color: Colors.red[100],
-              margin: const EdgeInsets.all(8),
-              child: ListView.builder(
-                itemCount: 3,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (_, j) => Container(
-                  width: 100,
-                  color: Colors.blue,
-                  margin: const EdgeInsets.all(8),
-                  child: Center(child: Text('$i-$j')),
-                ),
+          child: NotificationListener<OuterScrollNotification>(
+            onNotification: (notification) {
+              // 接收來自內層的滾動請求
+              _outerController.jumpTo(
+                _outerController.offset + notification.delta,
+              );
+              return true;
+            },
+            child: ListView.builder(
+              controller: _outerController,
+              itemCount: 5,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (_, i) => Container(
+                width: 300,
+                color: Colors.red[100],
+                margin: const EdgeInsets.all(8),
+                child: _InnerScrollView(index: i),
               ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _InnerScrollView extends StatefulWidget {
+  final int index;
+  
+  const _InnerScrollView({required this.index});
+
+  @override
+  State<_InnerScrollView> createState() => _InnerScrollViewState();
+}
+
+class _InnerScrollViewState extends State<_InnerScrollView> {
+  final ScrollController _innerController = ScrollController();
+  double _lastDragPosition = 0;
+  bool _isDragging = false;
+  bool _isControllingOuter = false;
+
+  @override
+  void dispose() {
+    _innerController.dispose();
+    super.dispose();
+  }
+
+  void _handleDragStart(DragStartDetails details) {
+    _isDragging = true;
+    _isControllingOuter = false;
+    _lastDragPosition = details.globalPosition.dx;
+  }
+
+  void _handleDragUpdate(DragUpdateDetails details) {
+    if (!_isDragging) return;
+
+    final delta = details.globalPosition.dx - _lastDragPosition;
+    _lastDragPosition = details.globalPosition.dx;
+
+    if (_isControllingOuter) {
+      // 發送通知給外層
+      OuterScrollNotification(-delta).dispatch(context);
+      return;
+    }
+
+    final innerPosition = _innerController.position;
+    final atLeftEdge = innerPosition.pixels <= innerPosition.minScrollExtent;
+    final atRightEdge = innerPosition.pixels >= innerPosition.maxScrollExtent;
+
+    if ((delta > 0 && atLeftEdge) || (delta < 0 && atRightEdge)) {
+      _isControllingOuter = true;
+      OuterScrollNotification(-delta).dispatch(context);
+    } else {
+      final newOffset = _innerController.offset - delta;
+      final clampedOffset = newOffset.clamp(
+        innerPosition.minScrollExtent,
+        innerPosition.maxScrollExtent,
+      );
+      _innerController.jumpTo(clampedOffset);
+    }
+  }
+
+  void _handleDragEnd(DragEndDetails details) {
+    _isDragging = false;
+    _isControllingOuter = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onHorizontalDragStart: _handleDragStart,
+      onHorizontalDragUpdate: _handleDragUpdate,
+      onHorizontalDragEnd: _handleDragEnd,
+      child: ListView.builder(
+        controller: _innerController,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: 6,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (_, j) => Container(
+          width: 100,
+          color: Colors.blue,
+          margin: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '${widget.index}-$j',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                if (j == 0)
+                  const Icon(
+                    Icons.arrow_back,
+                    color: Colors.white54,
+                    size: 16,
+                  ),
+                if (j == 5)
+                  const Icon(
+                    Icons.arrow_forward,
+                    color: Colors.white54,
+                    size: 16,
+                  ),
+              ],
             ),
           ),
         ),
